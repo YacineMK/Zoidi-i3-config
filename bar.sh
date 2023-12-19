dfe65efe7597de6371e44a79506daca9e9a02bc1
YacineMK <merroukiyacine06@gmail.com>
2023-12-19 20:38:58 +0100

    fix bar and window gaps

--- bar.sh
+++ bar.sh
@@ -5,16 +5,45 @@ while true; do
     date_str=$(date +"%d/%m/%Y")
 
     # Get current time
-    time_str=$(date +"%H:%M:%S")
+    time_str=$(date +"%H:%M")
 
     # Get battery percentage
-    battery_percentage=$(acpi | awk '{print $4}' | tr -d '%,')
-    
+    battery_info=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage:")
+    battery_percentage=$(echo "$battery_info" | grep -oP '\d+' | head -n1)
+
+    # Get battery status
+    battery_status=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "state:" | awk '{print $2}')
+
+    # Convert battery status to an icon
+    if [ "$battery_status" == "charging" ]; then
+        battery_icon=" "
+    elif [ "$battery_status" == "discharging" ]; then
+        battery_icon=" "
+    fi
+
     # Get CPU usage percentage
     cpu_percentage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | awk -F. '{print $1}')
 
-    # Print the formatted bar
-    echo "%{c}Date: $date_str | Time: $time_str | Battery: $battery_percentage% | CPU: $cpu_percentage%"
+    # Get temperature (assuming it's available in the thermal_zone0)
+    temperature=$(cat /sys/class/thermal/thermal_zone0/temp)
+    temperature_celsius=$((temperature/1000))
+
+    # Set the color based on battery percentage
+    if [ "$battery_percentage" -lt 25 ]; then
+        battery_color="%{F#FFA500}"  # Orange color
+    else
+        battery_color="%{F-}"  # Default color
+    fi
+
+    # Set the color based on temperature threshold
+    if [ "$temperature_celsius" -ge 80 ]; then
+        temp_color="%{F#FF0000}"  # Red color
+    else
+        temp_color="%{F-}"  # Default color
+    fi
+
+    # Print the formatted bar with numeric battery percentage, color, and temperature
+    echo "$date_str |  $time_str |   $battery_icon $battery_percentage% Battery |  $cpu_percentage% CPU |  $temperature_celsius°C  "
 
     # Sleep for a short duration (adjust as needed)
     sleep 1
